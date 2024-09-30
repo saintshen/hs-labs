da4b16ffae7da8367b0c7e6864d1c338e6920349
Ming Shen <saintshen@gmail.com>
2024-09-29 22:03:43 -0400

    cfip

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import { createRequestHandler } from "@remix-run/express";
 import express from "express";
 import morgan from "morgan";
-import path  from "path";
+import path from "path";
 import * as rfs from "rotating-file-stream";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const viteDevServer =
   process.env.NODE_ENV === "production"
@@ -14,31 +14,33 @@ const viteDevServer =
         })
       );
 
-
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // create a rotating write stream
-var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d', // rotate daily
-  path: path.join(__dirname, 'log')
-})
-app.use(morgan("[:date[clf]] :req['cf-connecting-ip'] :req['X-Real-IP'] :method :url :status :res[content-length] :response-time ms :referrer :user-agent", { stream: accessLogStream }));
+var accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotate daily
+  path: path.join(__dirname, "log"),
+});
+
+morgan.token("cfip", function (req, res) {
+  return req.headers["cf-connection-ip"];
+});
+app.use(
+  morgan(
+    "[:date[clf]] :cfip :method :url :status :res[content-length] :response-time ms :referrer :user-agent",
+    { stream: accessLogStream }
+  )
+);
 
 app.use(
-  viteDevServer
-    ? viteDevServer.middlewares
-    : express.static("build/client")
+  viteDevServer ? viteDevServer.middlewares : express.static("build/client")
 );
 
 const build = viteDevServer
-  ? () =>
-      viteDevServer.ssrLoadModule(
-        "virtual:remix/server-build"
-      )
+  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
   : await import("./build/server/index.js");
-
 
 app.all("*", createRequestHandler({ build }));
 
